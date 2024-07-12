fae08bb14d1b58838bff93ea8bf748ae12088698
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-07-12 17:05:24 -0400

    Add plot for SS3 and BAM sb

---
+++ R/plot_spawning_biomass.R
@@ -0,0 +1,103 @@
+plot_spawning_biomass <- function(dat,
+                         model,
+                         refpts = FALSE,
+                         warnings = FALSE) {
+  # if(warnings){
+  #   suppressWarnings()
+  # }
+
+  if (model == "SS3") {
+    # load SS3 data file
+    # check if dat parameter is a report file or a mutates df
+    if(grepl("Report.sso", dat)){
+      get_ncol <- function(file, skip = 0) {
+        nummax <- max(utils::count.fields(file,
+                                          skip = skip, quote = "",
+                                          comment.char = ""
+        )) + 1
+        return(nummax)
+      }
+
+      output <- read.table(
+        file = dat, col.names = 1:get_ncol(dat), fill = TRUE, quote = "",
+        colClasses = "character", nrows = -1, comment.char = "",
+        blank.lines.skip = FALSE
+      )
+    } else {
+      output <- dat
+    }
+
+    bio_info <- SS3_extract_df(output, "DERIVED_QUANTITIES")[-c(1:4),]
+    colnames(bio_info) <- bio_info[1,]
+    bio_info <- bio_info[-1,] |>
+      tidyr::separate_wider_delim(cols = LABEL, delim = "_", names = c("label", "year"))
+
+    # Extract spawning biomass from data
+    biomass_info <- SS3_extract_bio_info(dat = dat, parameter = "SPB")
+    # Extract B0 from DERIVED_QUANITIES df
+    # B0 <- as.numeric(biomass_info[['Value']][biomass_info[['year']]=='Virgin'])
+    biomass <- biomass_info |>
+      dplyr::mutate(year = as.numeric(year),
+                    value = as.numeric(Value),
+                    stddev = as.numeric(StdDev)) |>
+      dplyr::select(year, value, stddev) |>
+      dplyr::filter(year!='Virgin' & year!="Initial")
+
+    # Extract reference points, Btarget, Unfished biomass, and Bmsy
+    tgt <- as.numeric(bio_info[['Value']][bio_info[['label']]=="SSB" & bio_info[['year']]=="Btgt"])
+    unfished <- as.numeric(bio_info[['Value']][bio_info[['label']]=="SSB" & bio_info[['year']]=="Unfished"])
+    msy <- as.numeric(bio_info[['Value']][bio_info[['label']]=="SSB" & bio_info[['year']]=="MSY"])
+
+    # sbr <- SS3_extract_df(dat, "Spawning_Biomass_Report_1")[-c(1:3),]
+    # colnames(sbr) <- c("year", "era", "value")
+    # sbr <- sbr |>
+    #   dplyr::filter(era!='VIRG' & era!="INIT") |>
+    #   dplyr::mutate(year = as.numeric(year),
+    #                 value = as.numeric(value))
+    # ggplot2::ggplot(dat = sbr) +
+    #   ggplot2::geom_line(ggplot2::aes(x = year, y = value/1000, color = era), linewidth = 1)
+
+    plt <- ggplot2::ggplot(data = biomass) +
+      ggplot2::geom_line(ggplot2::aes(x = year, y = value/1000), linewidth = 1) +
+      ggplot2::geom_ribbon(ggplot2::aes(x = year, ymin = (value/1000 - stddev/1000), ymax = (value/1000 + stddev/1000)), colour = "grey", alpha = 0.3) +
+      ggplot2::geom_hline(yintercept = unfished/1000, linetype = 2) +
+      ggplot2::geom_hline(yintercept = tgt/1000, linetype = 3) +
+      ggplot2::geom_hline(yintercept = msy/1000, linetype = 4) +
+      ggplot2::labs(x = "Year",
+                    y = "Spawning Biomass (metric tons)") +
+      ggplot2::theme_classic()
+  } # close SS3 if statement
+
+  if (model == "BAM"){
+    # extract spawning biomass from output
+    output <- dget(dat)
+    sb <- data.frame(sapply(output$t.series, c))|>
+      dplyr::select(year, SSB)
+
+    # Projection years
+    proj <- data.frame(sapply(output$projection, c)) |>
+      dplyr::select(year, SSB.proj) |>
+      dplyr::rename(SSB = SSB.proj)
+
+    biomass <- rbind(sb, proj) |>
+      dplyr::rename(value = SSB)
+
+    # Pull reference pts
+    unfished <- output$parms$SSB0
+    msy <- output$parms$SSBmsy
+    tgt <- output$parms$SSB.F30 # SSBF30 but calling tgt to use same plotting for all
+
+    plt <- ggplot2::ggplot(data = biomass) +
+      ggplot2::geom_line(ggplot2::aes(x = year, y = value/1000), linewidth = 1) +
+      # ggplot2::geom_ribbon(ggplot2::aes(x = year, ymin = (value/1000 - stddev/1000), ymax = (value/1000 + stddev/1000)), colour = "grey", alpha = 0.3) +
+      ggplot2::geom_hline(yintercept = unfished/1000, linetype = 2) +
+      ggplot2::geom_hline(yintercept = tgt/1000, linetype = 3) +
+      ggplot2::geom_hline(yintercept = msy/1000, linetype = 4) +
+      ggplot2::labs(x = "Year",
+                    y = "Spawning Biomass (metric tons)") +
+      ggplot2::theme_classic()
+
+  }
+
+  return(plt)
+}

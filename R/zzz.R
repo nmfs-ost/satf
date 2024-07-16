810dc8c8ee1752dc711a6e6c0472b9286e74a8e8
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-07-16 14:42:34 -0400

    Add directions for actions on start up when loading pkg

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+### Start up messages and actions to run for use of satf
+# Start up message
+
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Thanks for using satf! Please visit the GitHub pages for more information on this package as well as latest developments. If you have any issues or inquires, please use the Issues page on GitHub (https://github.com/nmfs-ost/satf/issues)")
+}
+
+# Things to load on start up
+.onLoad <- function(libname, pkgname){
+  # import NOAA approved fonts from package
+  suppressWarnings(
+    extrafont::font_import(pattern = c("cambria", "arial"), prompt = FALSE)
+  )
+
+}

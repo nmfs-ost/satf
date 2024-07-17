68dd4df6ce5c2b1d1936856913c9e4763b28bd2d
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-07-17 16:51:15 -0400

    Update plot so SS3 output extraction works

--- R/plot_recruitment.R
+++ R/plot_recruitment.R
@@ -23,8 +23,25 @@ plot_recruitment <- function(dat,
                              include_fxn = FALSE,
                              show_warnings = FALSE){
   if(model == "SS3"){
+    # Read rep file or rename
+    if(grepl("Report.sso", dat)){
+      get_ncol <- function(file, skip = 0) {
+        nummax <- max(utils::count.fields(file,
+                                          skip = skip, quote = "",
+                                          comment.char = ""
+        )) + 1
+        return(nummax)
+      }
+
+      output <- utils::read.table(
+        file = dat, col.names = 1:get_ncol(dat), fill = TRUE, quote = "",
+        nrows = -1, comment.char = "", blank.lines.skip = FALSE
+      )
+    } else {
+      output <- dat
+    }
     # extract recruitment
-    sr_info <- SS3_extract_df(dat, "SPAWN_RECRUIT")
+    sr_info <- SS3_extract_df(output, "SPAWN_RECRUIT")
     sr <- sr_info[-c(1:13),]
     colnames(sr) <- sr_info[12,]
     sr <- sr  |>
@@ -37,15 +54,16 @@ plot_recruitment <- function(dat,
                     biasadj = as.numeric(biasadj))
 
     # Store virgin and initial recruitment for reference in plotting
-    time_series <- SS3_extract_df(dat, "TIME_SERIES")
+    time_series <- SS3_extract_df(output, "TIME_SERIES")
     colnames(time_series) <- time_series[2,]
     time_series <- time_series[-c(1:2),]
+    time_series[, 5:ncol(time_series)] <- lapply(5:ncol(time_series), function(x) suppressWarnings(as.numeric(time_series[[x]])))
+
     # Adapted from r4ss
     B0 <- sum(time_series[["SpawnBio"]][time_series[["Era"]] == "VIRG"], na.rm = TRUE)
     B1 <- sum(time_series[["SpawnBio"]][time_series[["Era"]] == "INIT"], na.rm = TRUE)
     R0 <- sum(time_series[["Recruit_0"]][time_series[["Era"]] == "VIRG"], na.rm = TRUE)
     R1 <- sum(time_series[["Recruit_0"]][time_series[["Era"]] == "INIT"], na.rm = TRUE)
-
 
     # extract sr_err
     sr_err <- sr_info[10:11,]
@@ -83,7 +101,7 @@ plot_recruitment <- function(dat,
 
     # need to rescale to 1000s rather than xe..
     sr_plt <- ggplot2::ggplot(data = sr2) +
-      ggplot2::geom_line(ggplot2::aes(x = spawn_bio/1000, y = exp_recr/1000)) + # exp. R
+      ggplot2::geom_line(ggplot2::aes(x = spawn_bio/1000, y = exp_recr/1000), linewidth = 1) + # exp. R
       # add exp R after bias adjustment (dotted line)
       ggplot2::geom_point(ggplot2::aes(x = spawn_bio/1000, y = pred_recr/1000, color = year)) + # change colors
       # ggplot2::geom_text() +
@@ -92,13 +110,14 @@ plot_recruitment <- function(dat,
       ggplot2::theme_classic() +
       ggplot2::theme(legend.position = "none")
 
-    if(include_fxn){
-      # identify SR fxn
-      sr_plt <- sr_plt +
-        ggplot2::geom_annotate("text", x = 1, y = 1, label = paste())
-    }
+    sr_plt <- add_theme(sr_plt)
 
     rts <- ggplot2::ggplot()
-  }
+  } # close SS3 if statement
+
+  if(model == "BAM"){
+
+  } # close BAM if statement
+  return(sr_plt)
 }
 

1cf4cae1db35927671f4382b8b68d744f4e06216
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-09-25 16:29:58 -0400

    Add plot_landings

--- R/plot_landings.R
+++ R/plot_landings.R
@@ -1,16 +1,43 @@
-plot_land <- function(dat,
-                      model){
-  output <- dat |> dplyr::filter(label == "landings")
+plot_landings <- function(dat,
+                          model = "standard",
+                          units = NULL){
+  land <- utils::read.csv(dat) |> 
+    dplyr::filter(module_name == "t.series" | module_name == "CATCH",
+                  grepl("landings", label) | label == "obs") |>
+    dplyr::mutate(estimate = as.numeric(estimate),
+                  year = as.numeric(year))
 
-  narea <- length(unique(output$area))
-  nseas <- length(unique(output$season))
+  land_ts <- land |>
+    dplyr::filter(!is.na(year),
+                  !is.na(fleet),
+                  label == "landings_observed" | label == "obs") 
 
-  if (narea > 1 | nseas > 1) {
+  narea <- length(unique(land_ts$area))
+  nseas <- length(unique(land_ts$season))
 
-  } else {
-    plt <- ggplot2::ggplot(data = output) +
-      ggplot2::geom_line(ggplot2::aes(x = time, y = label))+
-      ggplot2::facet_wrap(~fleet)
+  if(narea > 1) {
+    factors <- TRUE
   }
+  if (nseas > 1) {
+    land_ts2 <- land_ts |>
+      dplyr::group_by(year, fleet, sex, area, growth_pattern) |>
+      dplyr::summarize(estimate = mean(estimate)) |>
+      dplyr::mutate(fleet = as.character(fleet))
+  }
+
+
+  plt <- ggplot2::ggplot(data = land_ts2) +
+    ggplot2::geom_area(ggplot2::aes(x = year, y = estimate, fill = fleet))
+
   add_theme(plt)
+
+
 }
+
+# Std framework examples
+# SS3
+dat <- "C:/Users/samantha.schiano/Documents/Stock Assessment Workflow/test_ASAR/billfish_output.csv"
+# BAM
+#dat <- utils::read.csv("C:/Users/samantha.schiano/Documents/Stock Assessment Workflow/test_ASAR/blk_sea_bass_output.csv")[,-1]
+dat <- "C:/Users/samantha.schiano/Documents/Stock Assessment Workflow/test_ASAR/black_sea_bass_output.csv"
+

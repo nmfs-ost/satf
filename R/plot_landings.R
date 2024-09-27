9625f2f89d2c340cae089a81e8327aa90f0a8449
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-09-27 14:08:19 -0400

    Add notes to code

--- R/plot_landings.R
+++ R/plot_landings.R
@@ -11,8 +11,13 @@
 plot_landings <- function(dat,
                           model = "standard",
                           units = NULL){
+  # check to make sure file works with fxn
+  if (grepl(".sso|.rdat", dat)) {
+    stop("File type not compatible with function. Please use standard csv format of output files. An example can be found at https://github.com/nmfs-ost/satf")
+  }
+  # read standard data file and extract target quantity
   land <- utils::read.csv(dat) |>
-    dplyr::filter(module_name == "t.series" | module_name == "CATCH",
+    dplyr::filter(module_name == "t.series" | module_name == "CATCH", # t.series is associated with a conversion from BAM output and CATCH with SS3 converted output
                   grepl("landings", label) | label == "obs") |>
     dplyr::mutate(estimate = as.numeric(estimate),
                   year = as.numeric(year))
@@ -22,25 +27,26 @@ plot_landings <- function(dat,
                   !is.na(fleet),
                   label == "landings_observed" | label == "obs")
 
+  #Check number of areas and season - if any are >1 then need to use alternative plot (or summarize)
   narea <- length(unique(land_ts$area))
   nseas <- length(unique(land_ts$season))
 
   if(narea > 1) {
     factors <- TRUE
+    # will need facet if TRUE
   }
+  # Check for nseas > 1 - mean of landings through the year
   if (nseas > 1) {
-    land_ts2 <- land_ts |>
+    land_ts <- land_ts |>
       dplyr::group_by(year, fleet, sex, area, growth_pattern) |>
       dplyr::summarize(estimate = mean(estimate)) |>
       dplyr::mutate(fleet = as.character(fleet))
   }
 
-
-  plt <- ggplot2::ggplot(data = land_ts2) +
+  # Make generic plot
+  plt <- ggplot2::ggplot(data = land_ts) +
     ggplot2::geom_area(ggplot2::aes(x = year, y = estimate, fill = fleet))
-
+  # Apply std NOAA theme
   add_theme(plt)
-
-
 }
 

6a2d094b852d30d07c017ffd05a7fb91bc7ca635
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2024-09-29 20:27:29 -0700

    feat(hake): Adds data object for Big hake
    called hake.
    The data was added programatically using usethis, which is now included
    as a suggested dependency.

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' Example `dat` object using Big Hake
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `who`
+#' A data frame with 26,4713 rows and 31 columns:
+#' \describe{
+#'   \item{label} {}
+#'   \item{estimate} {}
+#'   \item{year} {}
+#'   \item{fleet} {}
+#'   \item{sex} {}
+#'   \item{area} {}
+#'   \item{growth_pattern} {}
+#'   \item{module_name} {}
+#'   \item{uncertainty_label} {}
+#'   \item{uncertainty} {}
+#'   \item{time} {}
+#'   \item{season} {}
+#'   \item{subseason} {}
+#'   \item{age} {}
+#'   \item{len_bins} {}
+#'   \item{initial} {}
+#'   \item{likelihood} {}
+#'   \item{gradient} {}
+#'   \item{estimated} {}
+#'   \item{bio_pattern} {}
+#'   \item{birthseas} {}
+#'   \item{settlement} {}
+#'   \item{morph} {}
+#'   \item{type} {}
+#'   \item{factor} {}
+#'   \item{platoon} {}
+#'   \item{month} {}
+#'   \item{sexes} {}
+#'   \item{part} {}
+#'   \item{bin} {}
+#'   \item{kind} {}
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"hake"

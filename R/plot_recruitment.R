6664ebda9230a62d49484fef7dbb36e56354cb14
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2024-09-30 21:23:54 -0700

    fix(plot_spawning_biomass): Remove complications
    Just have a standard plot and not all the if else statements by using
    simple ifelse statements in the code. Also, removed plots for non-standard
    output per Sam's guidance.
    
    Changes to all plotting and tables
    * dat is an object rather than reading in the file every time a figure or table
      is created, this change is carried over throughout the documentation and the
      code in all functions.
    * Removed SS3 and BAM methods
    
    Changes to `plot_spawning_biomass()`
    * There was a problem with `if(any(grepl("target", output$label)))` because it
      ignored what the user specified in `ref_line` if target is present in label,
      i.e., you could never get to MSY. There should be a two-level check, i.e.,
      what does the user want and is it present. I have not fixed this latter
      problem.
    * Fixed use of `..., ]$estimate` to `..., "estimate"]` because you should not
      use the dollar sign which allows for partial matches.
    * Use `switch()` statements instead of nested `if` `else` statements. Actually,
      now the function is so simple that I do not even have any switch statements
      but they can be helpful instead of nested if else statements.
    * Fixed the non-relative plots to truncate data to only use years less than end_yr
    * Some of the scaled items were hard-coded to 1000 instead of using scale_amount
    * Removed show_warnings because it was not actually doing anything
    * Use end_year rather than renaming it to endyr
    * Subset sb to end year before determining the correct number of breaks
    * MSY does not work so I removed it from the default reference list, only
      target and unfished work
    
    Need to fix:
      * units are not divided by scale amount to create new unit label
      * Why do we think that our determination of breaks is better than what ggplot2
        provides?
      * There is no example

--- R/plot_recruitment.R
+++ R/plot_recruitment.R
@@ -1,7 +1,6 @@
 #' Plot Recruitment
 #'
-#' @param dat .csv file and path where the data is located of standard stock assessment output
-#' @param model the model in which the output came from (standard, SS3, BAM, ect)
+#' @param dat A data frame returned from `asar::convert_output()`.
 #' @param params Print/export the parameters of the stock recruitment function?
 #' @param params_only Only export the stock recruitment function or both the parameters and the plot(s)?
 #' @param units If units are not available in the output file, in metric tons,
@@ -19,9 +18,7 @@
 #' reference line, stock recruitment curve, and other related figures.
 #' @export
 #'
-
 plot_recruitment <- function(dat,
-                             model = "standard",
                              params = FALSE,
                              params_only = FALSE,
                              units = c(sb = "metric tons", recruitment = "metric tons"),
@@ -46,70 +43,10 @@ plot_recruitment <- function(dat,
     sbu <- "metric tons"
   }
 
-  if (model == "standard"){
-    output <- utils::read.csv(dat)
-    if (scaled) {
-      rec <- output |>
-        dplyr::filter(label == "recruitment",
-                      module_name == "TIME_SERIES" | module_name == "t.series",
-                      !is.na(year),
-                      is.na(fleet) | length(unique(fleet)) <= 1,
-                      is.na(sex) | length(unique(sex)) <= 1,
-                      is.na(area) | length(unique(area)) <= 1,
-                      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
-                      year != "S/Rcurve" | year != "Init" | year != "Virg"
-        ) |> # SS3 and BAM target module names
-        dplyr::mutate(estimate = as.numeric(estimate),
-                      year = as.numeric(year)) |>
-        dplyr::rename(recruitment = estimate) |>
-        dplyr::select(-c(module_name, label))
-      sb <- output |>
-        dplyr::filter(label == "spawning_biomass",
-                      module_name == "TIME_SERIES" | module_name == "t.series",
-                      !is.na(year),
-                      is.na(fleet) | length(unique(fleet)) <= 1,
-                      is.na(sex) | length(unique(sex)) <= 1,
-                      is.na(area) | length(unique(area)) <= 1,
-                      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
-                      year != "S/Rcurve" | year != "Init" | year != "Virg"
-        ) |> # SS3 and BAM target module names
-        dplyr::mutate(estimate = as.numeric(estimate),
-                      year = as.numeric(year)) |>
-        dplyr::rename(spawning_biomass = estimate) |>
-        dplyr::select(-c(module_name, label))
-    } else {
-      rec <- output |>
-        dplyr::filter(label == "recruitment",
-                      module_name == "TIME_SERIES" | module_name == "t.series",
-                      !is.na(year),
-                      is.na(fleet) | length(unique(fleet)) <= 1,
-                      is.na(sex) | length(unique(sex)) <= 1,
-                      is.na(area) | length(unique(area)) <= 1,
-                      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
-                      year != "S/Rcurve" | year != "Init" | year != "Virg"
-        ) |> # SS3 and BAM target module names
-        dplyr::mutate(estimate = as.numeric(estimate),
-                      year = as.numeric(year)) |>
-        dplyr::rename(recruitment = estimate) |>
-        dplyr::select(-c(module_name, label))
-      sb <- output |>
-        dplyr::filter(label == "spawning_biomass",
-                      module_name == "TIME_SERIES" | module_name == "t.series",
-                      !is.na(year),
-                      is.na(fleet) | length(unique(fleet)) <= 1,
-                      is.na(sex) | length(unique(sex)) <= 1,
-                      is.na(area) | length(unique(area)) <= 1,
-                      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
-                      year != "S/Rcurve" | year != "Init" | year != "Virg"
-        ) |> # SS3 and BAM target module names
-        dplyr::mutate(estimate = as.numeric(estimate),
-                      year = as.numeric(year)) |>
-        dplyr::rename(spawning_biomass = estimate) |>
-        dplyr::select(-c(module_name, label))
-    }
-
-    rec_devs <- output |>
-      dplyr::filter(label == "recruitment_deviations" | label == "log_recruitment_deviations",
+  output <- dat
+  if (scaled) {
+    rec <- output |>
+      dplyr::filter(label == "recruitment",
                     module_name == "TIME_SERIES" | module_name == "t.series",
                     !is.na(year),
                     is.na(fleet) | length(unique(fleet)) <= 1,
@@ -120,203 +57,111 @@ plot_recruitment <- function(dat,
       ) |> # SS3 and BAM target module names
       dplyr::mutate(estimate = as.numeric(estimate),
                     year = as.numeric(year)) |>
-      dplyr::rename(recruitment_deviations = estimate) |>
+      dplyr::rename(recruitment = estimate) |>
       dplyr::select(-c(module_name, label))
-    if(return == "recruitment_deviations" & nrow(rec_devs)==0){
-      stop("No recruitment deviations found in data.")
-    }
+    sb <- output |>
+      dplyr::filter(label == "spawning_biomass",
+                    module_name == "TIME_SERIES" | module_name == "t.series",
+                    !is.na(year),
+                    is.na(fleet) | length(unique(fleet)) <= 1,
+                    is.na(sex) | length(unique(sex)) <= 1,
+                    is.na(area) | length(unique(area)) <= 1,
+                    is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+                    year != "S/Rcurve" | year != "Init" | year != "Virg"
+      ) |> # SS3 and BAM target module names
+      dplyr::mutate(estimate = as.numeric(estimate),
+                    year = as.numeric(year)) |>
+      dplyr::rename(spawning_biomass = estimate) |>
+      dplyr::select(-c(module_name, label))
+  } else {
+    rec <- output |>
+      dplyr::filter(label == "recruitment",
+                    module_name == "TIME_SERIES" | module_name == "t.series",
+                    !is.na(year),
+                    is.na(fleet) | length(unique(fleet)) <= 1,
+                    is.na(sex) | length(unique(sex)) <= 1,
+                    is.na(area) | length(unique(area)) <= 1,
+                    is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+                    year != "S/Rcurve" | year != "Init" | year != "Virg"
+      ) |> # SS3 and BAM target module names
+      dplyr::mutate(estimate = as.numeric(estimate),
+                    year = as.numeric(year)) |>
+      dplyr::rename(recruitment = estimate) |>
+      dplyr::select(-c(module_name, label))
+    sb <- output |>
+      dplyr::filter(label == "spawning_biomass",
+                    module_name == "TIME_SERIES" | module_name == "t.series",
+                    !is.na(year),
+                    is.na(fleet) | length(unique(fleet)) <= 1,
+                    is.na(sex) | length(unique(sex)) <= 1,
+                    is.na(area) | length(unique(area)) <= 1,
+                    is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+                    year != "S/Rcurve" | year != "Init" | year != "Virg"
+      ) |> # SS3 and BAM target module names
+      dplyr::mutate(estimate = as.numeric(estimate),
+                    year = as.numeric(year)) |>
+      dplyr::rename(spawning_biomass = estimate) |>
+      dplyr::select(-c(module_name, label))
+  }
 
-    if (is.null(end_year)){
-      endyr <- max(rec$year)
-    } else {
-      endyr <- end_year
-    }
-    stryr <- min(rec$year)
+  rec_devs <- output |>
+    dplyr::filter(label == "recruitment_deviations" | label == "log_recruitment_deviations",
+                  module_name == "TIME_SERIES" | module_name == "t.series",
+                  !is.na(year),
+                  is.na(fleet) | length(unique(fleet)) <= 1,
+                  is.na(sex) | length(unique(sex)) <= 1,
+                  is.na(area) | length(unique(area)) <= 1,
+                  is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+                  year != "S/Rcurve" | year != "Init" | year != "Virg"
+    ) |> # SS3 and BAM target module names
+    dplyr::mutate(estimate = as.numeric(estimate),
+                  year = as.numeric(year)) |>
+    dplyr::rename(recruitment_deviations = estimate) |>
+    dplyr::select(-c(module_name, label))
+  if(return == "recruitment_deviations" & nrow(rec_devs)==0){
+    stop("No recruitment deviations found in data.")
+  }
 
-    # merge DF
-    sr <- dplyr::full_join(sb, rec)
+  if (is.null(end_year)){
+    endyr <- max(rec$year)
+  } else {
+    endyr <- end_year
+  }
+  stryr <- min(rec$year)
 
-    # Choose number of breaks for x-axis
-    x_n_breaks <- round(length(subset(sr, year<=endyr)$year)/10)
-    if (x_n_breaks <= 5) {
-      x_n_breaks <- round(length(subset(sr, year<=endyr)$year)/5)
-    }
+  # merge DF
+  sr <- dplyr::full_join(sb, rec)
 
-    if (return == "stock_recruitment"){
-      plt <- ggplot2::ggplot(data = sr) +
-        ggplot2::geom_line(ggplot2::aes(x = spawning_biomass, y = recruitment/1000), linewidth = 1) +
-        ggplot2::labs(x = paste("Spawning Biomass (", sbu, ")", sep = ""),
-                      y = paste("Recruitment (", ru, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none")
-      # sr_plt <- add_theme(sr_plt)
-    } else if (return == "recruitment") {
-      plt <- ggplot2::ggplot(data = sr) +
-        ggplot2::geom_point(ggplot2::aes(x = year, y = recruitment)) +
-        ggplot2::geom_line(ggplot2::aes(x = year, y = recruitment), linewidth = 1) +
-        ggplot2::labs(x = "Year",
-                      y = paste("Recruitment (", ru, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none") +
-        ggplot2::scale_x_continuous(n.breaks = x_n_breaks,
-                                    guide = ggplot2::guide_axis(minor.ticks = TRUE))
-    } else if (return == "recruitment_deviations") {
-      plt <- ggplot2::ggplot(data = rec_devs) +
-        # ggplot2::geom_point(ggplot2::aes(x = year, y = log_rec_dev), shape = 1, size = 2.5) +
-        ggplot2::geom_pointrange(ggplot2::aes(x = year, y = estimate, ymax = Value, ymin = 0),  fatten = 1, size = 2, shape = 1) +
-        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-        ggplot2::labs(x = "Year",
-                      y = "Recruitment Deviations")
-    }
-    plt_fin <- add_theme(plt)
-  } else if(model == "SS3") {
-    # Read rep file or rename
-    if(grepl(".sso", dat)){
-      get_ncol <- function(file, skip = 0) {
-        nummax <- max(utils::count.fields(file,
-                                          skip = skip, quote = "",
-                                          comment.char = ""
-        )) + 1
-        return(nummax)
-      }
+  # Choose number of breaks for x-axis
+  x_n_breaks <- round(length(subset(sr, year<=endyr)$year)/10)
+  if (x_n_breaks <= 5) {
+    x_n_breaks <- round(length(subset(sr, year<=endyr)$year)/5)
+  }
 
-      output <- utils::read.table(
-        file = dat, col.names = 1:get_ncol(dat), fill = TRUE, quote = "",
-        nrows = -1, comment.char = "", blank.lines.skip = FALSE
-      )
-    } else {
-      output <- dat
-    }
-    # extract recruitment
-    sr_info <- SS3_extract_df(output, "SPAWN_RECRUIT")
-    sr <- sr_info[-c(1:14),]
-    colnames(sr) <- tolower(sr_info[13,])
-    sr <- sr  |>
-      dplyr::mutate(year = yr,
-                    spawnbio = as.numeric(spawnbio),
-                    exp_recr = as.numeric(exp_recr),
-                    with_regime = as.numeric(with_regime),
-                    bias_adjusted = as.numeric(bias_adjusted),
-                    pred_recr = as.numeric(pred_recr),
-                    # dev = as.numeric(dev),
-                    # biasadjuster = as.numeric(biasadjuster)
-                    )
-
-    # Store virgin and initial recruitment for reference in plotting
-    time_series <- SS3_extract_df(output, "TIME_SERIES")
-    colnames(time_series) <- tolower(time_series[2,])
-    time_series <- time_series[-c(1:2),]
-    time_series[, 5:ncol(time_series)] <- lapply(5:ncol(time_series), function(x) suppressWarnings(as.numeric(time_series[[x]])))
-
-    # Adapted from r4ss
-    B0 <- sum(time_series[["spawnbio"]][time_series[["era"]] == "VIRG"], na.rm = TRUE)
-    B1 <- sum(time_series[["spawnbio"]][time_series[["era"]] == "INIT"], na.rm = TRUE)
-    R0 <- sum(time_series[["recruit_0"]][time_series[["era"]] == "VIRG"], na.rm = TRUE)
-    R1 <- sum(time_series[["recruit_0"]][time_series[["era"]] == "INIT"], na.rm = TRUE)
-
-    # Units - change this statement to fit new convention
-    sb_units = spawning_biomass_units
-    rec_units = recruitment_units
-    message("Default units for both SB and R are metric tons.")
-
-    # remove inital and virg recruitment
-    sr <- sr |>
-      dplyr::filter(year!="Init" & year!="Virg" & era != "Forecast") |>
-      dplyr::mutate(year = as.numeric(year))
-
-    if (return == "stock_recruitment") {
-      # need to rescale to 1000s rather than xe.
-      plt <- ggplot2::ggplot(data = sr) +
-        ggplot2::geom_line(ggplot2::aes(x = spawnbio/1000, y = exp_recr/1000), linewidth = 1) + # exp. R
-        # add exp R after bias adjustment (dotted line)
-        ggplot2::geom_point(ggplot2::aes(x = spawnbio/1000, y = pred_recr/1000, color = year)) + # change colors
-        # ggplot2::geom_text() +
-        ggplot2::labs(x = paste("Spawning Biomass (", sb_units, ")", sep = ""),
-             y = paste("Recruitment (", rec_units, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none")
-    } else if (return == "recruitment") {
-      plt <- ggplot2::ggplot(data = sr) +
-        ggplot2::geom_point(ggplot2::aes(x = year, y = pred_recr)) +
-        ggplot2::geom_line(ggplot2::aes(x = year, y = pred_recr), linewidth = 1) +
-        ggplot2::labs(x = "Year",
-                      y = paste("Recruitment (", rec_units, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none")
-    } else if (return == "recruitment_deviations") {
-      # recruitment deviations
-      params <- SS3_extract_df(output, "PARAMETERS")
-      colnames(params) <- params[2,]
-      params2 <- params[-c(1:2),] |>
-        dplyr::filter(grepl('RecrDev', Label)) |>
-        dplyr::select(Label, Value) |>
-        tidyr::separate_wider_delim(Label, delim = "_", names = c("Era", "Recr", "Year"))
-      params_proj <-  params[-c(1:2),] |>
-        dplyr::filter(grepl('ForeRecr', Label)) |>
-        dplyr::select(Label, Value) |>
-        tidyr::separate_wider_delim(Label, delim = "_", names = c("Recr", "Year")) |>
-        dplyr::mutate(Era = "Fore")
-
-      params_fin <- rbind(params2, params_proj)
-
-      sr2 <- dplyr::left_join(sr, params_fin, by = c("yr" = "Year")) |>
-        dplyr::mutate(Value = as.numeric(Value))
-
-      plt <- ggplot2::ggplot(data = sr2) +
-        # ggplot2::geom_point(ggplot2::aes(x = year, y = log_rec_dev), shape = 1, size = 2.5) +
-        ggplot2::geom_pointrange(ggplot2::aes(x = year, y = exp_recr, ymax = Value, ymin = 0),  fatten = 1, size = 2, shape = 1) +
-        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-        ggplot2::labs(x = "Year",
-                      y = "logR Deviations")
-    }
-    plt_fin <- add_theme(plt)
-  } # close SS3 if statement
-
-  if(model == "BAM"){
-    # read in output
-    output <- dget(dat)
-
-    R_virg <- output$parms$R.virgin.bc
-    R0 <- output$parms$R0
-    SSBmsy <- output$parms$SSBmsy
-    SSB0 <- output$parms$SSB0
-
-    # SSB time series
-    sr <- data.frame(year = output$t.series$year,
-                     spawn_bio = output$t.series$SSB,
-                     pred_recr = output$t.series$recruits,
-                     # exp_recr = output$t.series,
-                     log_rec_dev = output$t.series$logR.dev)
-
-    # Check if units were declared
-    sb_units = spawning_biomass_units
-    rec_units = recruitment_units
-    message("Default units for both SB and R are metric tons.")
-
-    if (return == "stock_recruitment") {
-      # plot stock recruitment in 1000
-      plt <- ggplot2::ggplot(data = sr) +
-        # ggplot2::geom_line(ggplot2::aes(x = spawn_bio, y = exp_recr), linewidth = 1) + # exp. R
-        # add exp R after bias adjustment (dotted line)
-        ggplot2::geom_point(ggplot2::aes(x = spawn_bio, y = pred_recr, color = year)) + # change colors
-        # ggplot2::geom_text() +
-        ggplot2::labs(x = paste("Spawning Biomass (", sb_units, ")", sep = ""),
-                      y = paste("Recruitment (", rec_units, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none")
-    } else if (return == "recruitment") {
-      plt <- ggplot2::ggplot(data = sr) +
-        ggplot2::geom_line(ggplot2::aes(x = year, y = pred_recr), linewidth = 1) + # exp. R
-        ggplot2::geom_point(ggplot2::aes(x = year, y = pred_recr)) + # change colors
-        # ggplot2::geom_text() +
-        ggplot2::labs(x = "Year",
-                      y = paste("Recruitment (", rec_units, ")", sep = "")) +
-        ggplot2::theme(legend.position = "none")
-    } else if (return == "recruitment_deviations") {
-      plt <- ggplot2::ggplot(data = sr) +
-        # ggplot2::geom_point(ggplot2::aes(x = year, y = log_rec_dev), shape = 1, size = 2.5) +
-        ggplot2::geom_pointrange(ggplot2::aes(x = year, y = log_rec_dev, ymax = log_rec_dev, ymin = 0),  fatten = 1, size = 2, shape = 1) +
-        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-        ggplot2::labs(x = "Year",
-                      y = "logR Deviations")
-    }
-    plt_fin <- add_theme(plt)
-  } # close BAM if statement
+  if (return == "stock_recruitment"){
+    plt <- ggplot2::ggplot(data = sr) +
+      ggplot2::geom_line(ggplot2::aes(x = spawning_biomass, y = recruitment/1000), linewidth = 1) +
+      ggplot2::labs(x = paste("Spawning Biomass (", sbu, ")", sep = ""),
+                    y = paste("Recruitment (", ru, ")", sep = "")) +
+      ggplot2::theme(legend.position = "none")
+    # sr_plt <- add_theme(sr_plt)
+  } else if (return == "recruitment") {
+    plt <- ggplot2::ggplot(data = sr) +
+      ggplot2::geom_point(ggplot2::aes(x = year, y = recruitment)) +
+      ggplot2::geom_line(ggplot2::aes(x = year, y = recruitment), linewidth = 1) +
+      ggplot2::labs(x = "Year",
+                    y = paste("Recruitment (", ru, ")", sep = "")) +
+      ggplot2::theme(legend.position = "none") +
+      ggplot2::scale_x_continuous(n.breaks = x_n_breaks,
+                                  guide = ggplot2::guide_axis(minor.ticks = TRUE))
+  } else if (return == "recruitment_deviations") {
+    plt <- ggplot2::ggplot(data = rec_devs) +
+      # ggplot2::geom_point(ggplot2::aes(x = year, y = log_rec_dev), shape = 1, size = 2.5) +
+      ggplot2::geom_pointrange(ggplot2::aes(x = year, y = estimate, ymax = Value, ymin = 0),  fatten = 1, size = 2, shape = 1) +
+      ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+      ggplot2::labs(x = "Year",
+                    y = "Recruitment Deviations")
+  }
+  plt_fin <- add_theme(plt)
   return(plt_fin)
 }
-
--- R/table_indices.R
+++ R/table_indices.R
@@ -6,153 +6,28 @@
 #' @export
 #'
 
-table_indices <- function(dat,
-                          model = "standard"){
-  if (model == "standard"){
-    output <- utils::read.csv(dat)
+table_indices <- function(dat) {
+  output <- dat
+  output <- output |>
+    dplyr::filter(module_name == "INDEX_2" | module_name == "t.series")
+  if (any(unique(output$module_name=="INDEX_2"))) {
     output <- output |>
-      dplyr::filter(module_name == "INDEX_2" | module_name == "t.series")
-    if (any(unique(output$module_name=="INDEX_2"))) {
-      output <- output |>
-        dplyr::filter(grepl("obs", label))
-    } else if (any(unique(output$module_name=="t.series"))) {
-      output <- output |>
-        dplyr::filter(grepl("cpue", label))
-    }
-    fleet_names <- unique(output$fleet)
-    factors <- c("year", "fleet", "fleet_name", "age", "sex", "area", "seas", "season", "time", "era", "subseas", "subseason", "platoon", "platoo","growth_pattern", "gp")
-    # re-structure df for table
-    indices <- output |>
-      dplyr::rename(!!unique(output$label) := estimate,
-                    !!unique(output$uncertainty_label) := uncertainty) |>
-      tidyr::pivot_wider(
-        id_cols = -intersect(colnames(output), factors),
-        names_from = fleet,
-        values_from = c(unique(output$label), unique(output$uncertainty_label))
-      ) # stated internal error for tidyr and asks to report - try again monday
-
-  } else if (model == "SS3"){
-    # Read report file
-    if(grepl(".sso|.txt", dat)){
-      get_ncol <- function(file, skip = 0) {
-        nummax <- max(utils::count.fields(file,
-                                          skip = skip, quote = "",
-                                          comment.char = ""
-        )) + 1
-        return(nummax)
-      }
-
-      output <- utils::read.table(
-        file = dat, col.names = 1:get_ncol(dat), fill = TRUE, quote = "",
-        colClasses = "character", nrows = -1, comment.char = "",
-        blank.lines.skip = FALSE
-      )
-    }
-    # Extract Fleet Names
-    fleet_info <- SS3_extract_df(output, "Fleet")[,1]
-    colnames(fleet_info) <- fleet_info[1,1]
-    fleet_info <- fleet_info[-1,]
-    fleet_names <- unlist(as.vector(fleet_info))
-    fleet_names <- paste("Fleet_", fleet_names, sep ="")
-    # fleet_names <- as.character(fleet_info[fleet_info$X1=="fleet_names:",][,-1])
-    # fleet_names <- gsub("_", " ", fleet_names)
-
-    # Index 1
-    index1 <- SS3_extract_df(output, "INDEX_1")
-    colnames(index1) <- index1[2,]
-    index1 <- index1[-c(1:2),]
-
-    # Index 2
-    index2 <- SS3_extract_df(output, "INDEX_2")
-    colnames(index2) <- index2[2,]
-    index2 <- index2[-c(1:2),] |>
-      dplyr::select(Fleet, Yr, Seas, Area, Subseas, Month, Obs, SE, Exp) |>
-      dplyr::mutate(Obs = round(as.numeric(Obs), digits = 3),
-                    SE = round(as.numeric(SE), digits = 3),
-                    Exp = round(as.numeric(Exp), digits = 3))
-    index2_obs <- index2 |>
-      tidyr::pivot_wider(id_cols = Yr,
-                         names_from = Fleet,
-                         values_from = Obs) |>
-      dplyr::rename_at(dplyr::vars(-1), ~ paste(., "obs", sep = "_"))
-    index2_se <- index2 |>
-      tidyr::pivot_wider(id_cols = Yr,names_from = Fleet, values_from = SE) |>
-      dplyr::rename_at(dplyr::vars(-1), ~ paste(., "se", sep = "_"))
-
-    # ind_fleets <- unique(index2$Fleet) |>
-    #   strsplit(split = "_")
-    ind_fleets <- paste("Fleet_", unique(index2$Fleet), sep = "")
-
-    # Recombine fleet names - header name ready
-    # select_one <- function(x){
-    #   a <- lapply(x, tail, -1)
-    #   sapply(a, paste, collapse = " ")
-    # }
-    #
-    # ind_fleets <- select_one(ind_fleets)
-
-    indices <- merge(index2_obs, index2_se) |>
-      dplyr::rename(year = Yr)
-    indices <- indices[, sort(names(indices))] |>
-      dplyr::select("year", dplyr::everything())
-    indices[is.na(indices)] <- "-"
-
-    tab <- indices |>
-      flextable::flextable() |>
-      flextable::set_header_labels(values = c(
-        "Year", rep(c("Obs.", "SE"),
-                    length(ind_fleets))
-      )) |>
-      flextable::add_header_row(top = TRUE, values = c(
-        "", rep(intersect(ind_fleets,fleet_names), each = 2))
-      )
-    tab <- add_theme(tab)
-
-
-  } # close SS3 if statement
-
-  if(model == "BAM"){
-    output <- dget(dat)
-    indices <- output$t.series |>
-      dplyr::select(year, dplyr::contains("U.") & contains(".ob") | contains("cv.U"))
-
-    # Create function to reorder column names so ordered by fleet
-    fleet_names <- function(x){
-      extract_names <- colnames(x)[-1] |>
-        stringr::str_replace("U.", "") |>
-        stringr::str_replace("cv.", "") |>
-        stringr::str_replace(".ob", "") |>
-        unique()
-      return(extract_names)
-    }
-    col_order <- function(x){
-      extract_names <- fleet_names(x)
-      out_df <- x |> dplyr::select(year)
-      for(i in 1:length(extract_names)){
-        cols <- grep(extract_names[i], names(x), value = TRUE)
-        cols_extract <- x[, cols]
-        out_df <- cbind(out_df, cols_extract)
-      }
-      return(out_df)
-    }
-
-    indices <- col_order(indices)
-    indices[is.na(indices)] <- "-"
-    ind_fleets <- fleet_names(indices)
-
-    tab <- indices |>
-      dplyr::mutate(year = as.factor(year)) |>
-      flextable::flextable() |>
-      flextable::set_header_labels(values = c(
-        "Year", rep(c("Obs.", "CV"),
-                    length(ind_fleets))
-      )) |>
-      flextable::add_header_row(top = TRUE, values = c(
-        "", rep(ind_fleets, each = 2))
-      )
-    tab <- add_theme(tab)
-
-  } # close BAM if statement
+      dplyr::filter(grepl("obs", label))
+  } else if (any(unique(output$module_name=="t.series"))) {
+    output <- output |>
+      dplyr::filter(grepl("cpue", label))
+  }
+  fleet_names <- unique(output$fleet)
+  factors <- c("year", "fleet", "fleet_name", "age", "sex", "area", "seas", "season", "time", "era", "subseas", "subseason", "platoon", "platoo","growth_pattern", "gp")
+  # re-structure df for table
+  indices <- output |>
+    dplyr::rename(!!unique(output$label) := estimate,
+                  !!unique(output$uncertainty_label) := uncertainty) |>
+    tidyr::pivot_wider(
+      id_cols = -intersect(colnames(output), factors),
+      names_from = fleet,
+      values_from = c(unique(output$label), unique(output$uncertainty_label))
+    ) # stated internal error for tidyr and asks to report - try again monday
 
   return(tab)
 }

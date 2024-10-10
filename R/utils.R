8c497f36742f74ec052f606aadeda9089c9560c5
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-10-10 16:08:51 -0400

    feat: Started making risk table based on AFSC's SAFE main table

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,19 @@
-# Comma: format table numbers
+#########################
+####    Utilities    ####
+#########################
 
-#' Function to format numbers to text with 1,000s of commas.
+# -----format table numbers-----
+
+#' Format numbers to text with 1,000s of commas.
 #' This function was copied with permission from Ben Williams,
 #' a NOAA Research Fisheries Biologist, as part of the AFSC SAFE
 #' reporting tool.
 #'
 #' @param number value to format
-#'
-#' @export comma
+#' @noRd
 #'
 comma <- function( number ) {
   format(round(as.numeric( number ), digits = 0), big.mark = ",")
 }
+
+# ----- -----

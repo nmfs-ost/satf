cdff6eb98e5c422108c64436af47f354f6ae13cd
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-10-17 14:00:47 -0400

    build: created first formal draft of exp_fig_accessible.R function, with documentation.

--- R/exp_fig_accessible.R
+++ R/exp_fig_accessible.R
@@ -1,35 +1,69 @@
+#' Export figure and alternative text
+#'
+#' Function to export a specified plot object, and its alternative
+#' text, from the R environment.
+#'
+#' @param figure Plot object in the R environment to export.
+#' @param alt_text Alternative text for the figure.
+#' @param path Directory in which "exported" folder should be saved.
+#' @param width Plot width, in units (see argument below). Default
+#' is 5.
+#' @param height Plot height, in units (see argument below). Default
+#' is 5.
+#' @param units Plot size units (options: "in", "cm", "mm", "px").
+#' Default is "cm".
 
-# create figure with alt text placeholder
-fig1 <- satf::plot_spawning_biomass(
-  Petrale_sole_std_res_2023
-  ) +
-  ggplot2::labs(
-    caption = "my caption"
-  )
+#' @return A folder containing exported plots and associated
+#'  alternative text as .jpg and .csv objects, respectively.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' model_data <- read.csv(here::here("data/Petrale_sole_std_res_2023.csv"))
+#'
+#' fig1 <- satf::plot_spawning_biomass(model_data)
+#'
+#' exp_fig_accessible(figure = fig1,
+#'                    alt_text = "my alt text",
+#'                    path = here::here(),
+#'                    width = 5,
+#'                    height = 5,
+#'                    units = "cm")
+#'
+#' }
 
-# view figure
-fig1
+exp_fig_accessible <- function(figure,
+                               alt_text,
+                               path,
+                               width = 5,
+                               height = 5,
+                               units = "cm"){
 
-# add alt text
-fig1_alttext <- "alt text here"
-fig1 <- fig1 +
-  ggplot2::labs(
-    alt = fig1_alttext
-  )
+  # turn figure name into a character string so it can be
+  # pasted into filepath
+  fig_name_deparsed <- sprintf(deparse(substitute(figure)))
 
-# view figure with alt text (shouldn't be any different)
-fig1
+  # create new "exported" folder
+  new_path <- paste0(path, "/", "exported")
+  dir.create(path = new_path)
 
-# check alt text was added to figure
-ggplot2::get_alt_text(fig1)
+  # export figure to jpg
+  ggplot2::ggsave(filename = paste0(fig_name_deparsed, ".jpg"),
+                  plot = figure,
+                  path = new_path,
+                  width = width,
+                  height = height,
+                  units = units
+                  )
 
-# export figure to jpg
-ggplot2::ggsave(filename = "test.jpg",
-                plot = fig1)
+  # make template csv to store alt text
+  alt_text_df <- data.frame("Figure" = as.character(fig_name_deparsed),
+                            "Alt_text" = as.character(alt_text))
 
-# make template csv
-alt_text_df <- data.frame("Figure" = as.character(quote(fig1)),
-                          "Alt_text" = as.character(fig1_alttext))
-# export alt text to csv
-write.csv(alt_text_df,
-          "test.csv")
+  # export alt text csv
+  write.csv(x = alt_text_df,
+            file = paste0(new_path, "/", fig_name_deparsed, "_alt_text.csv"),
+            row.names=FALSE)
+
+}

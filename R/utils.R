e11854a9b97eb2e9dedd83b08b66449416a952f4
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-11-13 14:50:40 -0500

    converted code involved in exporting the rda to its own function within utils, then updated plot_landings, recruitment, and spawning_biomass

--- R/utils.R
+++ R/utils.R
@@ -30,4 +30,23 @@ extract_caps_alttext <- function(topic_label){
   return(caps_alttext_list)
 }
 
+# export ggplot, alt text, and caption to rda if indicated in arguments
+export_rda <- function(plt_fin = plt_fin,
+                       caps_alttext = caps_alttext,
+                       rda_folder = rda_folder,
+                       topic_label = topic_label){
 
+   rda <- list("figure" = plt_fin,
+              "cap" = caps_alttext[[1]],
+              "_alttext" = caps_alttext[[2]])
+
+  # check if an rda_files folder already exists; if not, make one
+  if (!dir.exists(file.path(rda_folder, "rda_files"))) {
+    dir.create(file.path(rda_folder, "rda_files"))
+  }
+
+  save(rda,
+       file = file.path(rda_folder,
+                        "rda_files",
+                        paste0(topic_label, "_rda.rda")))
+}

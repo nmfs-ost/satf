318658ef9dab11d665f95251260874701d8dd0f8
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-11-14 11:37:27 -0500

    build: updated alt text/captions csv to include caption and alt text for kobe plot. Also updated the key quantities section of create_template to include the variables to be extracted from the model results file in order to substitute placeholders in csv.

--- R/write_captions.R
+++ R/write_captions.R
@@ -25,12 +25,52 @@ write_captions <- function(dat, # converted model output object
   )
 
   # extract key quantities (these are examples and are not accurate)
+  # REMINDER: the variable names must exactly match those in the captions/alt text csv.
+  ## quantities applicable to multiple plots
   start_year <- min(as.numeric(dat$year[dat$year!="S/Rcurve" | dat$year!="Virg" | dat$year!="Init"]), na.rm = TRUE)
-                # as.numeric(dat[3,3])
+
+  # end_year
+
+
+  ## quantities for individual plots
+  ### kobe plot
+  # B_div_BMSY_min <- # (= minimum value of B/B(MSY))
+  # B_div_BMSY_max <- # (= maximum value of B/B(MSY))
+  # F_div_FMSY_min <- # (= minimum value of F/F(MSY))
+  # F_div_FMSY_max <- # (= maximum value of F/F(MSY))
+  # B_div_BMSY_end_yr <- # (= value of B/B(MSY) at the end year)
+  # F_div_FMSY_end_yr <- # (= value of F/F(MSY) at the end year)
+  # overfished_status_is_isnot <- # object that should be "is" or "is not" and answers the question, "the stock overfishing status ___ overfished"
+  # overfishing_status_is_isnot <- # object that should be "is" or "is not" and answers the question, "the stock ___ experiencing overfishing"
+
+  ### F
   Fend_df <- dat |>
     dplyr::filter(label == "fishing_mortality" & year == year | label == "F_terminal")
   Fend <- as.numeric(Fend_df$estimate)
-          # as.numeric(dat$estimate[2])
+
+  # Ftarg <-
+  # F_Ftarg <-
+
+  ### B
+  # Bend <-
+  # Btarg <-
+  # Bmsy <-
+
+
+  ### SB
+  # SBmsy <-
+  # fSB <-
+  # sbtarg <-
+
+  ### Other
+  # tot_catch <-
+  # M <-
+  # steep <-
+  # R0 <-
+
+
+
+
   # add in more quantities here, and update the quantities above
 
   # substitute quantity placeholders in the captions/alt text with

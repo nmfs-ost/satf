3ba9a71753ccfdc5147b2a909826a469936bbfb6
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-12-17 13:41:49 -0500

    [dev] table_bnd: update fxn and finish initial go through of table and export

--- R/table_bnc.R
+++ R/table_bnc.R
@@ -1,8 +1,29 @@
+#' Title
+#'
+#' @inheritParams plot_recruitment
+#' @param biomass_unit_label abbreviated units for biomass
+#' @param catch_unit_label abbreviated units for catch
+#'
+#' @return
+#' @export
+#'
+#' @examples
 table_bnc <- function(
     dat,
-    unit_label,
-    alt_label
+    end_year = NULL,
+    biomass_unit_label = "mt",
+    catch_unit_label = "mt",
+    make_rda = NULL
     ) {
+
+  biomass_label <- glue::glue("Biomass ({biomass_unit_label})")
+  catch_label <- glue::glue("Catch ({catch_unit_label})")
+
+  if (is.null(end_year)){
+    end_year <- {max(as.numeric(dat$year), na.rm = TRUE) - 10} |> suppressWarnings()
+  }
+
+  dat <- dplyr::filter(dat, year<=end_year)
 
   biomass <- dat |>
     dplyr::filter(
@@ -13,6 +34,7 @@ table_bnc <- function(
       is.na(fleet), is.na(sex), is.na(area), is.na(growth_pattern),
       module_name != "DERIVED_QUANTITIES"
     ) |>
+    dplyr::mutate(estimate = round(estimate, digits = 2)) |>
     dplyr::rename(biomass = estimate) |>
     dplyr::select(year, biomass)
 
@@ -23,35 +45,80 @@ table_bnc <- function(
   catch <- dat |>
     dplyr::filter(
       # SS3 params
-      grepl("catch", label),
-      !is.na(year),
+      grepl("catch$", label) | grepl("landings_observed", label),
+      !is.na(year), is.na(age),
       # module_name %in% c("TIME_SERIES","t.series"),
       # is.na(fleet), is.na(sex), is.na(area), is.na(growth_pattern),
       module_name != "DERIVED_QUANTITIES"
-    )
+    ) |>
+    dplyr::mutate(estimate = round(estimate, digits = 2))
   # Check if df is by age and summarize to time series
-  if (any(grepl("at_age", tolower(catch$module_name)))) {
+  if (length(unique(catch$year)) == nrow(catch)) {
     catch <- catch |>
-      dplyr::group_by(year) |>
-      dplyr::summarise(total_catch = sum(estimate))
+      dplyr::rename(total_catch = estimate) |>
+      dplyr::select(year, total_catch)
+  } else {
+    catch <- catch |>
+      # dplyr::filter(!is.na(estimate)) |>
+      dplyr::group_by(year) |> #, fleet
+      dplyr::summarise(total_catch = sum(estimate, na.rm = TRUE))
+    # if  ("fleet" %in% colnames(catch)) {
+    #   catch <- catch |>
+    #     tidyr::pivot_wider(
+    #       id_cols = year,
+    #       names_from = fleet,
+    #       values_from = total_catch
+    #     )
+    # }
   }
 
   abundance <- dat |>
     dplyr::filter(
       # SS3 params
-      grepl("mature_abundance", label), # | statement for BAM label
+      grepl("mature_abundance", label)| grepl("^abundance", label),
       !is.na(year),
       module_name %in% c("TIME_SERIES","t.series"),
       # is.na(fleet), is.na(sex), is.na(area), is.na(growth_pattern),
       module_name != "DERIVED_QUANTITIES"
-    ) |>
+    )
+  # Check if there is more than one year aka the values are factored
+  # TODO: Review that sum is OK to use in these cases - otherwise what are
+  #       the alternatives?
+  if(length(unique(abundance$year) != nrow(abundance))) {
+    abundance <- abundance |>
+      dplyr::group_by(year) |>
+      dplyr::summarise(estimate = sum(estimate))
+  }
+
+  abundance <- abundance |>
+    dplyr::mutate(estimate = round(estimate, digits = 0)) |>
     dplyr::rename(abundance = estimate) |>
     dplyr::select(year, abundance)
+
+  # Checks
+  if (length(unique(catch$year)) != nrow(catch)) warning("[catch] dataframe needs review.")
+  if (length(unique(biomass$year)) != nrow(biomass)) warning("[biomass] dataframe needs review.")
+  if (length(unique(abundance$year)) != nrow(abundance)) warning("[abundance] dataframe needs review.")
 
   # Bring together quantities for table
   bnc <- biomass |>
     dplyr::left_join(abundance, by = "year") |>
-    dplyr::left_join(catch, by = "year")
+    dplyr::left_join(catch, by = "year") |>
+    dplyr::mutate(year = as.factor(year)) |>
+    # apply table
+    flextable::flextable() |>
+    flextable::set_header_labels(
+      year = "Year",
+      biomass = biomass_label,
+      abundance = "Abundance",
+      total_catch = catch_label
+    )
+
+  # add theming to final table
+  tab_fin <- suppressWarnings(add_theme(bnc))
+
+  # create plot-specific variables to use throughout fxn for naming and IDing
+  topic_label <- "bnc"
 
   # identify output
   fig_or_table <- "table"
@@ -65,10 +132,28 @@ table_bnc <- function(
                          year = NULL)
   }
 
-  # REMINDERS: add in code that
+  # TODO: add in code that
   # -adds make_rda and rda_dir as arguments
   # -defines topic_label, caps_alttext; and
   # -makes an rda if make_rda = TRUE
   # (see table_indices.R for reference)
   # for the rda-related fxns to work, the final table has to be called tab
+
+  # extract this plot's caption and alt text
+  # caps_alttext <- extract_caps_alttext(topic_label = topic_label,
+  #                                      fig_or_table = fig_or_table)
+
+
+  # export figure to rda if argument = T
+  if (make_rda == TRUE){
+
+    export_rda(plt_fin = plt_fin,
+               # caps_alttext = caps_alttext,
+               rda_dir = rda_dir,
+               topic_label = topic_label,
+               fig_or_table = fig_or_table)
+
+  }
+  # Return finished table
+  tab_fin
 }

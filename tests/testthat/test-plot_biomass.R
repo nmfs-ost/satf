cc93de2338c7e3bf846b4288977d31249e68bb79
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2024-12-30 17:22:21 -0500

    created tests for plot_biomass

---
+++ tests/testthat/test-plot_biomass.R
@@ -0,0 +1,113 @@
+test_that("plot_biomass generates plots without errors", {
+
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("tests", "testthat", "fixtures", "sample_data", "petrale_sole-after_2020.csv", package = "satf")
+  )
+
+  # expect error-free plot with minimal arguments
+  expect_no_error(
+    satf::plot_biomass(dat)
+  )
+
+  # expect plot with warnings if ref_point not indicated
+  expect_warning(
+    satf::plot_biomass(dat)
+  )
+
+  # expect error-free plot with many arguments
+  expect_no_error(
+    satf::plot_biomass(
+      dat,
+      ref_point = 18000,
+      unit_label = "metric tons",
+      scale_amount = 1,
+      end_year = 2030,
+      relative = FALSE,
+      make_rda = FALSE,
+      rda_dir = getwd()
+    )
+  )
+
+  # expect error-free plot when setting relative to T
+  expect_no_error(
+    satf::plot_biomass(
+    dat,
+    ref_point = 18000,
+    unit_label = "metric tons",
+    scale_amount = 1,
+    end_year = 2030,
+    relative = TRUE,
+    make_rda = FALSE,
+    rda_dir = getwd()
+    )
+  )
+
+  # expect ggplot object is returned
+  expect_s3_class(
+    satf::plot_biomass(
+      dat,
+      ref_point = 18000,
+      unit_label = "metric tons",
+      scale_amount = 1,
+      end_year = 2030,
+      relative = TRUE,
+      make_rda = FALSE,
+      rda_dir = getwd()
+    ),
+    "gg"
+  )
+
+  })
+
+test_that("plot_biomass plots contain reference point when indicated", {
+
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("tests", "testthat", "fixtures", "sample_data", "petrale_sole-after_2020.csv", package = "satf")
+  )
+
+
+  # expect plot with a reference point (horizontal line) contains 4 layers while
+  # plot w/o ref pt contains only 3 layers
+
+  # make b plot with reference point
+  b_ref <- satf::plot_biomass(dat,
+                              ref_point = 18000)
+  # extract number of layers (should be 4)
+  b_ref_layers <- b_ref[["layers"]] |>
+    length()
+
+  # make b plot without reference point
+  b_no_ref <- satf::plot_biomass(dat)
+  # extract number of layers (should be 3)
+  b_no_ref_layers <- b_no_ref[["layers"]] |>
+    length()
+
+  expect_equal((b_ref_layers - 1),
+               b_no_ref_layers)
+
+
+})
+
+# test_that("rda file made when indicated",{
+#
+#   # read in sample dataset
+#   dat <- utils::read.csv(
+#     system.file("tests", "testthat", "fixtures", "sample_data", "petrale_sole-after_2020.csv", package = "satf")
+#   )
+#
+#   # export rda
+#   plot_biomass(
+#     dat,
+#     rda_dir = here::here(),
+#     make_rda = TRUE,
+#     end_year = 2023
+#   )
+#
+#   # expect that both rda_files dir and the biomass_figure.rda file exist
+#   expect_true(dir.exists(file.path(here::here(), "rda_files")))
+#   expect_true(file.exists(file.path(here::here(), "rda_files", "biomass_figure.rda")))
+#
+#
+# })

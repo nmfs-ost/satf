844ba27459e277e77b430011141b03a44eeabdfc
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-01-02 12:42:44 -0500

    Fix(write_captions): - Fix bug preventing key quantities from being substituted in alt text/captions csv. - Suppress warnings for write_captions

--- R/write_captions.R
+++ R/write_captions.R
@@ -28,6 +28,9 @@ write_captions <- function(dat, # converted model output object
   # REMINDERS:
   # -the variable names must exactly match those in the captions/alt text csv.
 
+  # suppress warnings
+  options(warn=-1)
+
   # FIGURES-----
 
   ## kobe plot
@@ -615,273 +618,282 @@ write_captions <- function(dat, # converted model output object
 
   # make list with all placeholders
   # uncomment placeholders once uncommented, above
-  patterns_replacements <- list(
+  patterns_replacements <- c(
     # FIGURES-----
 
    ## kobe plot
-   # 'B.BMSY.min' = B.BMSY.min,
-   # 'B.BMSY.max' = B.BMSY.max,
-   # 'F.FMSY.min' = F.FMSY.min,
-   # 'F.FMSY.max' = F.FMSY.max,
-   # 'B.BMSY.end.yr' = B.BMSY.end.yr,
-   # 'F.FMSY.end.yr' = F.FMSY.end.yr,
-   # 'overfished.status.is.isnot' = overfished.status.is.isnot,
-   # 'overfishing.status.is.isnot' = overfishing.status.is.isnot,
-   # 'kobe.start.year' = kobe.start.year,
-   # 'kobe.end.year' = kobe.end.year,
+   # 'B.BMSY.min' = as.character(as.character(B.BMSY.min),
+   # 'B.BMSY.max' = as.character(B.BMSY.max),
+   # 'F.FMSY.min' = as.character(F.FMSY.min),
+   # 'F.FMSY.max' = as.character(F.FMSY.max),
+   # 'B.BMSY.end.yr' = as.character(B.BMSY.end.yr),
+   # 'F.FMSY.end.yr' = as.character(F.FMSY.end.yr),
+   # 'overfished.status.is.isnot' = as.character(overfished.status.is.isnot),
+   # 'overfishing.status.is.isnot' = as.character(overfishing.status.is.isnot),
+   # 'kobe.start.year' = as.character(kobe.start.year),
+   # 'kobe.end.year' = as.character(kobe.end.year),
 
    ## Relative biomass plot
    # NOTE: moving this above biomass so rel.B.min isn't changed to "rel." + B.min (etc.)
-   'rel.B.min' = rel.B.min,
-   'rel.B.max' = rel.B.max,
+   'rel.B.min' = as.character(rel.B.min),
+   'rel.B.max' = as.character(rel.B.max),
 
    ## Biomass plot
-   # 'B.ref.pt' = B.ref.pt,
-   # 'B.ref.pt.units' = B.ref.pt.units,
-   'B.start.year' = B.start.year,
-   # 'B.end.year' = B.end.year,
-   # 'B.units' = B.units,
-   'B.min' = B.min,
-   'B.max' = B.max,
-   'R0' = R0,
-   # 'Bend' = Bend,
-   'Btarg' = Btarg,
-   # 'Bmsy' = Bmsy,
+   # 'B.ref.pt' = as.character(B.ref.pt),
+   # 'B.ref.pt.units' = as.character(B.ref.pt.units),
+   'B.start.year' = as.character(B.start.year),
+   # 'B.end.year' = as.character(B.end.year),
+   # 'B.units' = as.character(B.units),
+   'B.min' = as.character(B.min),
+   'B.max' = as.character(B.max),
+   'R0' = as.character(R0),
+   # 'Bend' = as.character(Bend),
+   'Btarg' = as.character(Btarg),
+   # 'Bmsy' = as.character(Bmsy),
 
     ## mortality (F) plot
-   # 'F.ref.pt' = F.ref.pt,
-   # 'F.ref.pt.units' = F.ref.pt.units,
-   # 'F.start.year' = F.start.year,
-   # 'F.end.year' = F.end.year,
-   # 'F.units' = F.units,
-   # 'F.min' = F.min,
-   # 'F.max' = F.max,
-   # 'Ftarg' = Ftarg,
-   # 'F.Ftarg' = F.Ftarg,
+   # 'F.ref.pt' = as.character(F.ref.pt),
+   # 'F.ref.pt.units' = as.character(F.ref.pt.units),
+   # 'F.start.year' = as.character(F.start.year),
+   # 'F.end.year' = as.character(F.end.year),
+   # 'F.units' = as.character(F.units),
+   # 'F.min' = as.character(F.min),
+   # 'F.max' = as.character(F.max),
+   # 'Ftarg' = as.character(Ftarg),
+   # 'F.Ftarg' = as.character(F.Ftarg),
 
     ## landings plot
-   'landings.start.year' = landings.start.year,
-   'landings.end.year' = landings.end.year,
-   # 'landings.units' = landings.units,
-   'landings.min' = landings.min,
-   'landings.max' = landings.max,
+   'landings.start.year' = as.character(landings.start.year),
+   'landings.end.year' = as.character(landings.end.year),
+   # 'landings.units' = as.character(landings.units),
+   'landings.min' = as.character(landings.min),
+   'landings.max' = as.character(landings.max),
 
    ## natural mortality (M)
-   # 'M.age.min' = M.age.min,
-   # 'M.age.max' = M.age.max,
-   # 'M.units' = M.units,
-   # 'M.rate.min' = M.rate.min,
-   # 'M.rate.max' = M.rate.max,
+   # 'M.age.min' = as.character(M.age.min),
+   # 'M.age.max' = as.character(M.age.max),
+   # 'M.units' = as.character(M.units),
+   # 'M.rate.min' = as.character(M.rate.min),
+   # 'M.rate.max' = as.character(M.rate.max),
 
    ## vonB LAA (von Bertalanffy growth function + length at age)
-   # 'vonb.age.units' = vonb.age.units,
-   # 'vonb.age.min' = vonb.age.min,
-   # 'vonb.age.max' = vonb.age.max,
-   # 'vonb.length.units' = vonb.length.units,
-   # 'vonb.length.min' = vonb.length.min,
-   # 'vonb.length.max' = vonb.length.max,
+   # 'vonb.age.units' = as.character(vonb.age.units),
+   # 'vonb.age.min' = as.character(vonb.age.min),
+   # 'vonb.age.max' = as.character(vonb.age.max),
+   # 'vonb.length.units' = as.character(vonb.length.units),
+   # 'vonb.length.min' = as.character(vonb.length.min),
+   # 'vonb.length.max' = as.character(vonb.length.max),
 
     ## length-type conversion plot
-   # 'total.length.units' = total.length.units,
-   # 'total.length.min' = total.length.min,
-   # 'total.length.max' = total.length.max,
-   # 'fork.length.units' = fork.length.units,
-   # 'fork.length.min' = fork.length.min,
-   # 'fork.length.max' = fork.length.max,
+   # 'total.length.units' = as.character(total.length.units),
+   # 'total.length.min' = as.character(total.length.min),
+   # 'total.length.max' = as.character(total.length.max),
+   # 'fork.length.units' = as.character(fork.length.units),
+   # 'fork.length.min' = as.character(fork.length.min),
+   # 'fork.length.max' = as.character(fork.length.max),
 
    ## weight-length conversion plot
-   # 'wl.length.units' = wl.length.units,
-   # 'wl.length.min' = wl.length.min,
-   # 'wl.length.max' = wl.length.max,
-   # 'wl.weight.units' = wl.weight.units,
-   # 'wl.weight.min' = wl.weight.min,
-   # 'wl.weight.max' = wl.weight.max,
+   # 'wl.length.units' = as.character(wl.length.units),
+   # 'wl.length.min' = as.character(wl.length.min),
+   # 'wl.length.max' = as.character(wl.length.max),
+   # 'wl.weight.units' = as.character(wl.weight.units),
+   # 'wl.weight.min' = as.character(wl.weight.min),
+   # 'wl.weight.max' = as.character(wl.weight.max),
 
    ## maturity schedule (proportion mature)
-   # 'prop.mat.length.units' = prop.mat.length.units,
-   # 'prop.mat.length.min' = prop.mat.length.min,
-   # 'prop.mat.length.max' = prop.mat.length.max,
+   # 'prop.mat.length.units' = as.character(prop.mat.length.units),
+   # 'prop.mat.length.min' = as.character(prop.mat.length.min),
+   # 'prop.mat.length.max' = as.character(prop.mat.length.max),
 
    ## fecundity at length
-   # 'fecundity.length.units' = fecundity.length.units,
-   # 'fecundity.length.min' = fecundity.length.min,
-   # 'fecundity.length.max' = fecundity.length.max,
-   # 'fecundity.units' = fecundity.units,
-   # 'fecundity.min' = fecundity.min,
-   # 'fecundity.max' = fecundity.max,
+   # 'fecundity.length.units' = as.character(fecundity.length.units),
+   # 'fecundity.length.min' = as.character(fecundity.length.min),
+   # 'fecundity.length.max' = as.character(fecundity.length.max),
+   # 'fecundity.units' = as.character(fecundity.units),
+   # 'fecundity.min' = as.character(fecundity.min),
+   # 'fecundity.max' = as.character(fecundity.max),
 
    ## CAA (catch at age)
-   # 'fleet.or.survey.name' = fleet.or.survey.name,
-   # 'caa.age.min' = caa.age.min,
-   # 'caa.age.max' = caa.age.max,
+   # 'fleet.or.survey.name' = as.character(fleet.or.survey.name),
+   # 'caa.age.min' = as.character(caa.age.min),
+   # 'caa.age.max' = as.character(caa.age.max),
 
    ## CAL (catch at length)
-   # 'cal.length.min' = cal.length.min,
-   # 'cal.length.max' = cal.length.max,
+   # 'cal.length.min' = as.character(cal.length.min),
+   # 'cal.length.max' = as.character(cal.length.max),
 
    ## CPUE indices plot
-   # 'cpue.start.year' = cpue.start.year,
-   # 'cpue.end.year' = cpue.end.year,
-   # 'cpue.units' = cpue.units,
-   # 'cpue.min' = cpue.min,
-   # 'cpue.max' = cpue.max,
+   # 'cpue.start.year' = as.character(cpue.start.year),
+   # 'cpue.end.year' = as.character(cpue.end.year),
+   # 'cpue.units' = as.character(cpue.units),
+   # 'cpue.min' = as.character(cpue.min),
+   # 'cpue.max' = as.character(cpue.max),
 
    ## NAA (numbers at age)
-   # 'bubble.start.year.min' = bubble.start.year.min,
-   # 'bubble.end.year.max' = bubble.end.year.max,
-   # 'bubble.age.units' = bubble.age.units,
-   # 'bubble.age.min' = bubble.age.min,
-   # 'bubble.age.max' = bubble.age.max,
+   # 'bubble.start.year.min' = as.character(bubble.start.year.min),
+   # 'bubble.end.year.max' = as.character(bubble.end.year.max),
+   # 'bubble.age.units' = as.character(bubble.age.units),
+   # 'bubble.age.min' = as.character(bubble.age.min),
+   # 'bubble.age.max' = as.character(bubble.age.max),
 
    ## mod_fit_catch (model fit to catch ts)
-   # 'mod.fit.catch.start.year' = mod.fit.catch.start.year,
-   # 'mod.fit.catch.end.year' = mod.fit.catch.end.year,
-   # 'mod.fit.catch.units' = mod.fit.catch.units,
-   # 'mod.fit.catch.min' = mod.fit.catch.min,
-   # 'mod.fit.catch.max' = mod.fit.catch.max,
+   # 'mod.fit.catch.start.year' = as.character(mod.fit.catch.start.year),
+   # 'mod.fit.catch.end.year' = as.character(mod.fit.catch.end.year),
+   # 'mod.fit.catch.units' = as.character(mod.fit.catch.units),
+   # 'mod.fit.catch.min' = as.character(mod.fit.catch.min),
+   # 'mod.fit.catch.max' = as.character(mod.fit.catch.max),
 
    ## mod_fit_abun (model fit to abundance indices plot)
-   # 'mod.fit.abun.start.year' = mod.fit.abun.start.year,
-   # 'mod.fit.abun.end.year' = mod.fit.abun.end.year,
+   # 'mod.fit.abun.start.year' = as.character(mod.fit.abun.start.year),
+   # 'mod.fit.abun.end.year' = as.character(mod.fit.abun.end.year),
 
    ## mod_fit_discards
-   # 'mod.fit.discards.start.year' = mod.fit.discards.start.year,
-   # 'mod.fit.discards.end.year' = mod.fit.discards.end.year,
-   # 'mod.fit.discards.units' = mod.fit.discards.units,
-   # 'mod.fit.discards.min' = mod.fit.discards.min,
-   # 'mod.fit.discards.max' = mod.fit.discards.max,
+   # 'mod.fit.discards.start.year' = as.character(mod.fit.discards.start.year),
+   # 'mod.fit.discards.end.year' = as.character(mod.fit.discards.end.year),
+   # 'mod.fit.discards.units' = as.character(mod.fit.discards.units),
+   # 'mod.fit.discards.min' = as.character(mod.fit.discards.min),
+   # 'mod.fit.discards.max' = as.character(mod.fit.discards.max),
 
    ## selectivity
-   # 'selectivity.start.year' = selectivity.start.year,
-   # 'selectivity.end.year' = selectivity.end.year,
-   # 'selectivity.length.units' = selectivity.length.units,
-   # 'selectivity.length.min' = selectivity.length.min,
-   # 'selectivity.length.max' = selectivity.length.max,
+   # 'selectivity.start.year' = as.character(selectivity.start.year),
+   # 'selectivity.end.year' = as.character(selectivity.end.year),
+   # 'selectivity.length.units' = as.character(selectivity.length.units),
+   # 'selectivity.length.min' = as.character(selectivity.length.min),
+   # 'selectivity.length.max' = as.character(selectivity.length.max),
 
    ## estimated stock recruitment (aka spawning stock biomass)
-   # 'sr.age.min' = sr.age.min,
-   # 'sr.ssb.units' = sr.ssb.units,
-   'sr.ssb.min' = sr.ssb.min,
-   'sr.ssb.max' = sr.ssb.max,
-   # 'sr.units' = sr.units,
-   'sr.min' = sr.min,
-   'sr.max' = sr.max,
+   # 'sr.age.min' = as.character(sr.age.min),
+   # 'sr.ssb.units' = as.character(sr.ssb.units),
+   'sr.ssb.min' = as.character(sr.ssb.min),
+   'sr.ssb.max' = as.character(sr.ssb.max),
+   # 'sr.units' = as.character(sr.units),
+   'sr.min' = as.character(sr.min),
+   'sr.max' = as.character(sr.max),
 
    # relative recruitment ts
    # NOTE: moving this above recruitment so rel.recruitment.min isn't changed
    # to "rel." + recruitment.min (etc.)
-   'rel.recruitment.min' = rel.recruitment.min,
-   'rel.recruitment.max' = rel.recruitment.max,
+   'rel.recruitment.min' = as.character(rel.recruitment.min),
+   'rel.recruitment.max' = as.character(rel.recruitment.max),
 
    ## recruitment ts
-   # 'recruitment.units' = recruitment.units,
-   'recruitment.start.year' = recruitment.start.year,
-   # 'recruitment.end.year' = recruitment.end.year,
-   'recruitment.min' = recruitment.min,
-   'recruitment.max' = recruitment.max,
+   # 'recruitment.units' = as.character(recruitment.units),
+   'recruitment.start.year' = as.character(recruitment.start.year),
+   # 'recruitment.end.year' = as.character(recruitment.end.year),
+   'recruitment.min' = as.character(recruitment.min),
+   'recruitment.max' = as.character(recruitment.max),
 
    ## recruitment deviations
-   'recruit.dev.start.year' = recruit.dev.start.year,
-   # 'recruit.dev.end.year' = recruit.dev.end.year,
-   'recruit.dev.min' = recruit.dev.min,
-   'recruit.dev.max' = recruit.dev.max,
+   'recruit.dev.start.year' = as.character(recruit.dev.start.year),
+   # 'recruit.dev.end.year' = as.character(recruit.dev.end.year),
+   'recruit.dev.min' = as.character(recruit.dev.min),
+   'recruit.dev.max' = as.character(recruit.dev.max),
 
    ## tot_b (total biomass)
-   # 'biomass.start.year' = biomass.start.year,
-   # 'biomass.end.year' = biomass.end.year,
-   # 'biomass.units' = biomass.units,
-   # 'biomass.min' = biomass.min,
-   # 'biomass.max' = biomass.max,
-   # 'biomass.ref.pt' = biomass.ref.pt,
-   # 'biomass.ref.pt.units' = biomass.ref.pt.units,
+   # 'biomass.start.year' = as.character(biomass.start.year),
+   # 'biomass.end.year' = as.character(biomass.end.year),
+   # 'biomass.units' = as.character(biomass.units),
+   # 'biomass.min' = as.character(biomass.min),
+   # 'biomass.max' = as.character(biomass.max),
+   # 'biomass.ref.pt' = as.character(biomass.ref.pt),
+   # 'biomass.ref.pt.units' = as.character(biomass.ref.pt.units),
 
    # relative ssb
    # NOTE: moving this above recruitment so rel.ssb.min isn't changed to
-   # "rel." + ssb.min, etc.
-   'rel.ssb.min' = rel.ssb.min,
-   'rel.ssb.max' = rel.ssb.max,
+   # "rel." + ssb.min), etc.
+   'rel.ssb.min' = as.character(rel.ssb.min),
+   'rel.ssb.max' = as.character(rel.ssb.max),
 
    ## spawning.biomass (ssb)
-   'ssb.start.year' = ssb.start.year,
-   # 'ssb.end.year' = ssb.end.year,
-   # 'ssb.units' = ssb.units,
-   'ssb.min' = ssb.min,
-   'ssb.max' = ssb.max#,
-   # 'ssb.ref.pt' = ssb.ref.pt,
-   # 'ssb.ref.pt.units' = ssb.ref.pt.units,
-   # 'ssbtarg' = ssbtarg,
+   'ssb.start.year' = as.character(ssb.start.year),
+   # 'ssb.end.year' = as.character(ssb.end.year),
+   # 'ssb.units' = as.character(ssb.units),
+   'ssb.min' = as.character(ssb.min),
+   'ssb.max' = as.character(ssb.max)#,
+   # 'ssb.ref.pt' = as.character(ssb.ref.pt),
+   # 'ssb.ref.pt.units' = as.character(ssb.ref.pt.units),
+   # 'ssbtarg' = as.character(ssbtarg),
 
 
    # ## spr (spawning potential ratio)
-   # 'spr.start.year' = spr.start.year,
-   # 'spr.end.year' = spr.end.year,
-   # 'spr.min' = spr.min,
-   # 'spr.max' = spr.max,
-   # 'spr.ref.pt' = spr.ref.pt,
-   # 'spr.ref.pt.units' = spr.ref.pt.units,
+   # 'spr.start.year' = as.character(spr.start.year),
+   # 'spr.end.year' = as.character(spr.end.year),
+   # 'spr.min' = as.character(spr.min),
+   # 'spr.max' = as.character(spr.max),
+   # 'spr.ref.pt' = as.character(spr.ref.pt),
+   # 'spr.ref.pt.units' = as.character(spr.ref.pt.units),
    #
    # ## pop_naa_baa (population numbers at age and population biomass at age)
-   # 'pop.naa.baa.start.year' = pop.naa.baa.start.year,
-   # 'pop.naa.baa.end.year' = pop.naa.baa.end.year,
-   # 'pop.naa.baa.fish.min' = pop.naa.baa.fish.min,
-   # 'pop.naa.baa.fish.max' = pop.naa.baa.fish.max,
+   # 'pop.naa.baa.start.year' = as.character(pop.naa.baa.start.year),
+   # 'pop.naa.baa.end.year' = as.character(pop.naa.baa.end.year),
+   # 'pop.naa.baa.fish.min' = as.character(pop.naa.baa.fish.min),
+   # 'pop.naa.baa.fish.max' = as.character(pop.naa.baa.fish.max),
    #
    # ## proj_catch (projected catch)
-   # 'proj.catch.units' = proj.catch.units,
-   # 'proj.catch.start.year' = proj.catch.start.year,
-   # 'proj.catch.end.year' = proj.catch.end.year,
-   # 'proj.catch.min' = proj.catch.min,
-   # 'proj.catch.max' = proj.catch.max,
+   # 'proj.catch.units' = as.character(proj.catch.units),
+   # 'proj.catch.start.year' = as.character(proj.catch.start.year),
+   # 'proj.catch.end.year' = as.character(proj.catch.end.year),
+   # 'proj.catch.min' = as.character(proj.catch.min),
+   # 'proj.catch.max' = as.character(proj.catch.max),
    #
    # ## proj_biomass (projected biomass)
-   # 'proj.biomass.units' = proj.biomass.units,
-   # 'proj.biomass.start.year' = proj.biomass.start.year,
-   # 'proj.biomass.end.year' = proj.biomass.end.year,
-   # 'proj.biomass.min' = proj.biomass.min,
-   # 'proj.biomass.max' = proj.biomass.max,
-   # 'proj.biomass.ref.pt' = proj.biomass.ref.pt,
-   # 'proj.biomass.ref.pt.units' = proj.biomass.ref.pt.units,
+   # 'proj.biomass.units' = as.character(proj.biomass.units),
+   # 'proj.biomass.start.year' = as.character(proj.biomass.start.year),
+   # 'proj.biomass.end.year' = as.character(proj.biomass.end.year),
+   # 'proj.biomass.min' = as.character(proj.biomass.min),
+   # 'proj.biomass.max' = as.character(proj.biomass.max),
+   # 'proj.biomass.ref.pt' = as.character(proj.biomass.ref.pt),
+   # 'proj.biomass.ref.pt.units' = as.character(proj.biomass.ref.pt.units),
    #
    # ## Other
-   # 'tot.catch' = tot.catch,
-   # 'M' = M,
-   # 'steep' = steep,
-   # 'SBmsy' = SBmsy,
-   # 'fSB' = fSB,
-   # 'sbtarg' = sbtarg,
+   # 'tot.catch' = as.character(tot.catch),
+   # 'M' = as.character(M),
+   # 'steep' = as.character(steep),
+   # 'SBmsy' = as.character(SBmsy),
+   # 'fSB' = as.character(fSB),
+   # 'sbtarg' = as.character(sbtarg),
    #
    # # TABLES-----
    #
    # ## catch
-   # 'catch.fleet' = catch.fleet,
+   # 'catch.fleet' = as.character(catch.fleet),
    #
    # ## landings
-   # 'landings.tbl.units' = landings.tbl.units,
+   # 'landings.tbl.units' = as.character(landings.tbl.units),
    #
    # ## discards
-   # 'discards.tbl.units' = discards.tbl.units,
+   # 'discards.tbl.units' = as.character(discards.tbl.units),
    #
    # ## catchability
-   # 'catchability.fleet' = catchability.fleet
+   # 'catchability.fleet' = as.character(catchability.fleet)
   )
 
   # take the values associated with the quantities and replace the df's
   # placeholders with them. For example, if ssb_min = 10, this will replace
   # "the minimum ssb = ssb_min" with "the minimum ssb = 10".
-  caps_alttext_subbed <- caps_alttext |>
-    dplyr::mutate(across(where(is.character), ~{
-      for (pattern in names(patterns_replacements)){
-        replacement_value <- patterns_replacements[[pattern]]
-        . <- stringr::str_replace_all(., pattern, as.character(replacement_value))
-      }
-      .
-    }))
+
+  # replace values in caption column
+  caps_alttext$caption <- stringr::str_replace_all(
+    caps_alttext$caption,
+    patterns_replacements
+  )
+
+  # replace values in alt text column
+  caps_alttext$alt_text <- stringr::str_replace_all(
+    caps_alttext$alt_text,
+    patterns_replacements
+  )
 
   # export df with updated captions and alt text to csv
- utils::write.csv(x = caps_alttext_subbed,
+ utils::write.csv(x = caps_alttext,
             file = fs::path(dir,
                              "captions_alt_text.csv"),
             row.names=FALSE)
+
+
+
+ # enable warnings again
+ options(warn=0)
 
 }
 

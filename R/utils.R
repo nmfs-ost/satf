ddb7c8de1291001a4623fc707ec76fa1a64c1afe
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-01-10 14:07:41 -0500

    first commit

--- R/utils.R
+++ R/utils.R
@@ -65,3 +65,49 @@ export_rda <- function(final = final,
                         "rda_files",
                         paste0(topic_label, "_", fig_or_table, ".rda")))
 }
+
+# substitute in more key quantities (units and end_years) to captions/alt text
+add_yr_units <- function(
+    topic = topic_label,
+    fig_or_table = fig_or_table,
+    end_year = NULL,
+    units = NULL){
+
+  # import csv
+  caps_alt_df <- read.csv(fs::path(getwd(), "captions_alt_text.csv"))
+
+  # make year character if not null
+  if (!is.null(end_year)){
+    end_year <- as.character(end_year)
+  }
+
+  # select specific fig/table's caption/alt text
+  topic_cap_alt <- caps_alt_df |>
+    dplyr::filter(label == topic,
+                  type == fig_or_table)
+
+  # select caption
+  topic_cap <- topic_cap_alt |>
+    dplyr::select(caption)
+
+  # select alt text
+  topic_alt <- topic_cap_alt |>
+    dplyr::select(alt_text)
+
+  # replace placeholders in alt text
+  ## end_year
+  # if "end.year" is found in topic_alt, replace it with end_year
+  topic_alt <- sub("\\S*end\\.year\\S*",
+                   as.character(end_year),
+                   topic_alt)
+
+  ## units
+  topic_alt <- sub("\\S*units\\S*",
+                   as.character(units),
+                   topic_alt)
+
+
+
+
+  }
+
